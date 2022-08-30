7f709d8c9d841cbd615c12585382c4f84520bf38
shailynch <shai.lynch.slgmail.com>
2022-08-30 11:02:08 +0100

    removing prepopulated data

--- src/main/resources/sql-data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `vet`.`customer` (`first_name`, `last_name`, `email`) VALUES ('shai', 'lynch', 'slynch\"qa.com');
-INSERT INTO `vet`.`pet` (`name`, `type`, `customer_id`, `dob`) VALUES ('Tom', 'tortoise', 1, '10102020');
