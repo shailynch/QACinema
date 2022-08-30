9cb0a5ac9dce6f1936cfaaed2d974d8f3f4e7b6d
shailynch <shai.lynch.slgmail.com>
2022-08-30 11:01:38 +0100

    sqldatat

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,46 +1,48 @@
-CREATE SCHEMA IF NOT EXISTS `vet`;
+CREATE SCHEMA IF NOT EXISTS `qacinema`;
 
-USE `vet` ;
+USE `qacinema` ;
 
-CREATE TABLE IF NOT EXISTS `vet`.`customer` (
+CREATE TABLE IF NOT EXISTS `qacinema`.`customer` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `last_name` VARCHAR(40) DEFAULT NULL,
     `email` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `vet`.`customer` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `last_name` VARCHAR(40) DEFAULT NULL,
-    `email` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `vet`.`vet` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `last_name` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-);
-
-CREATE TABLE IF NOT EXISTS `vet`.`pet` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(40) DEFAULT NULL,
-    `type` VARCHAR(40) DEFAULT NULL,
-    `customer_id` INT(11) DEFAULT NULL,
+    `mobile` VARCHAR(40) DEFAULT NULL,
     `dob` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
-
-CREATE TABLE IF NOT EXISTS `vet`.`appointment` (
+CREATE TABLE IF NOT EXISTS `qacinema`.`movie` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `customer_id` INT(11),
-    `pet_id` INT(11),
-    `vet_id` INT(11),
+    `title` VARCHAR(40) DEFAULT NULL,
+    `runtime` int(4) DEFAULT NULL,
+    `cast` VARCHAR(1000) DEFAULT NULL,
+    `release_date` VARCHAR(10) DEFAULT NULL,
+    `age_rating` VARCHAR(40) DEFAULT NULL,
+    `description` VARCHAR(500) DEFAULT NULL,
+    `poster_url` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
 
-INSERT INTO `vet`.`customer` (`first_name`, `last_name`, `email`) VALUES ('shai', 'lynch', 'slynch\"qa.com');
-INSERT INTO `vet`.`pet` (`name`, `type`, `customerid`) VALUES ('Tom', 'tortoise', 1);
+CREATE TABLE IF NOT EXISTS `qacinema`.`screen` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `capacity` int(4) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `qacinema`.`viewing` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `screen_id` INT(11) NOT NULL,
+    `movie_id` INT(11) NOT NULL,
+    `start_time` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS `qacinema`.`booking` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(11) NOT NULL,
+    `viewing_id` INT(11) NOT NULL,
+    `booking_date` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`id`)
+);
