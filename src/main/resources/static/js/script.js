3230e220249879a87fae24573f7d3b5b2f7d6914
shailynch <shai.lynch.slgmail.com>
2022-09-05 10:10:43 +0100

    updating customer film

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,31 +1,31 @@
 'use strict';
-const customerURL = "http://localhost:8088/customer";
+const customerURL = "http://localhost:8090/customer";
 
-const petURL = "http://localhost:8088/pet";
 
-const vetURL = "http://localhost:8088/vet";
-const _vet_first_name = document.querySelector("#vet_first_name");
-const _vet_last_name = document.querySelector("#vet_last_name");
-const _vet_email = document.querySelector("#vet_email");
-
-const appointmentURL = "http://localhost:8088/appointment";
-const _appointment_customer_id = document.querySelector("#appointment_customer_id");
-const _appointment_vet_id = document.querySelector("#appointment_vet_id");
-const _appointment_pet_id = document.querySelector("#appointment_pet_id");
+const bookingURL = "http://localhost:8090/booking";
+const _booking_customer_id = document.querySelector("#booking_customer_id");
+const _booking_vet_id = document.querySelector("#booking_viewing_id");
+const _booking_pet_id = document.querySelector("#booking_date_id");
 
 
 const addCustomer = () => {
 	const _first_name = document.querySelector("#first_name");
 	const _last_name = document.querySelector("#last_name");
 	const _email = document.querySelector("#email");
+	const _dob = document.querySelector("#dob");
+	const _mobile = document.querySelector("#mobile");
     const customerFirstName  = _first_name.value;
     const customerLastName  = _last_name.value;
-    const customerEmail = _email.value;
+    const customerEmail = _dob.value;
+    const customerDob  = _first_name.value;
+    const customerMobile  = _mobile.value;
     
     let data = { 
         "firstName": customerFirstName, 
         "lastName": customerLastName, 
-        "email": customerEmail 
+        "email": customerEmail,
+        "dob": customerDob,
+        "mobile": customerMobile 
     } 
     
 
@@ -44,69 +44,10 @@ const addCustomer = () => {
         .catch(err => console.error(`error ${err}`));
 };
 
-const addVet = () => {
-    const vetFirstName  = _vet_first_name.value;
-    const vetLastName  = _vet_last_name.value;
-    const vetEmail = _vet_email.value;
-    
-    let data = { 
-        "firstName": vetFirstName, 
-        "lastName": vetLastName, 
-        "email": vetEmail 
-    } 
-
-
-    fetch(`${vetURL}/add`, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-        .then(response => response.json())
-        .then(model => {
-            console.log(model);
-            allFromCustomer();
-        })
-        .catch(err => console.error(`error ${err}`));
-        
-        
-};
-
-const addPet = () => {
-	const _pet_name = document.querySelector("#pet_name");
-	const _pet_type = document.querySelector("#pet_type");
-	const _customer_id = document.querySelector("#customer_id");
-    const petName  = _pet_name.value;
-    const petType  = _pet_type.value;
-    const customerID = _customer_id.value;
-    
-    let data = { 
-        "name": petName, 
-        "type": petType, 
-        "customerID": customerID 
-    } 
-
-    fetch(`${petURL}/add`, {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-        .then(response => response.json())
-        .then(model => {
-            console.log(model);
-            allFromCustomer();
-        })
-        .catch(err => console.error(`error ${err}`));
-
-};
-
-const addAppointment = () => {
-    const petID = _appointment_pet_id.value;
-    const customerID = _appointment_customer_id.value;
-    const vetID = _appointment_vet_id.value;
+const addBooking = () => {
+    const petID = _booking_pet_id.value;
+    const customerID = _booking_customer_id.value;
+    const vetID = _booking_vet_id.value;
     
     let data = { 
         "petID": petID, 
@@ -114,7 +55,7 @@ const addAppointment = () => {
         "customerID": customerID 
     } 
 
-    fetch(`${appointmentURL}/add`, {
+    fetch(`${bookingURL}/add`, {
         method: "POST",
         body: JSON.stringify(data),
         headers: {
@@ -169,52 +110,4 @@ const updateCustomer = () => {
         .catch(err => console.error(`error ${err}`))
         console.log("fail");
         };
-        
- const updatePet = () => {
-	window.alert(`${petURL}/update/${_update_pet_id.value}`);
-	const _update_pet_name = document.querySelector("#update_pet_name");
-	const _update_pet_type = document.querySelector("#update_pet_type");
-	const _update_pet_customer_id = document.querySelector("#update_pet_customer_id");
-	const _update_pet_id = document.querySelector("#update_pet_id");
-   	const updatePetName  = _update_pet_name.value;
-    const updatePetType  = _update_pet_type.value;
-    const updatePetCustomerID = _update_pet_customer_id.value;
-	const updatePetID = _update_pet_id.value; 
-	
-
-    let data = { 
-		"id":  updatePetID,
-        "name": updatePetName , 
-        "type": updatePetType, 
-        "customerID": updatePetCustomerID  
-    } 
-    
-
-
-   fetch(`${customerURL}/update/${updateCustomerID}`, {
-     method: 'PUT',
-     body: JSON.stringify(data),
-      headers: {
-            "Content-Type": "application/json"
-        }
-    })
-        .then(response => response.json())
-        .then(model => {
-            console.log(model);
-            allFromCustomer();
-        })
-        .catch(err => console.error(`error ${err}`))
-        console.log("fail");
-        };
-
-const deleteCustomer = (customerID) => {
-    fetch(`${customerURL}/delete/${customerID}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-        .then(response => console.log(response))
-        .then(() => readAllTasks())
-        .catch(err => console.error(`error ${err}`));
-};
+ 
