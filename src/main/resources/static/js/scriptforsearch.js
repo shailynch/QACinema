f38dabedd2c195a745abd971de52949a7424a9ce
DanyelHarrison <107627845+DanyelHarrison@users.noreply.github.com>
2022-09-08 17:22:25 +0100

    Update scriptforsearch.js

--- src/main/resources/static/js/scriptforsearch.js
+++ src/main/resources/static/js/scriptforsearch.js
@@ -1,8 +1,8 @@
 document.querySelector('#search').addEventListener("click", getFilmByTitle);
 
 function getFilmByTitle(e){
-    const title = document.querySelector("#filmSearch").value;
-    fetch(`http://18.132.36.167:8080/movie/title/${title}`)
+    const id = document.querySelector("#filmSearch").value;
+    fetch(`http://18.132.36.167:8080/movie/${id}`)
     .then(res => res.json())
     .then((data) => {
         document.querySelector(".films").innerHTML = `
