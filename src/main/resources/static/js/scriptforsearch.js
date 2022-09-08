99ccb21091a17b331702e3de288134407e956c1a
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-09-08 15:23:46 +0100

    Styling added to search page

--- src/main/resources/static/js/scriptforsearch.js
+++ src/main/resources/static/js/scriptforsearch.js
@@ -1,12 +1,13 @@
 document.querySelector('#search').addEventListener("click", getFilmByTitle);
+const searchURL = "http://18.132.36.167:8080/";
 
 function getFilmByTitle(e){
-    const id = document.querySelector("#filmSearch").value;
-    fetch(`http://localhost:8090/movie/${title}`)
+    const title = document.querySelector("#filmSearch").value;
+    fetch(`${searchURL}/movie/${title}`)
     .then(res => res.json())
     .then((data) => {
         document.querySelector(".films").innerHTML = `
-<div class="filmInfo">
+    <div class="filmInfo">
     <h1>${data.title}</h1>
     <p>Runtime: ${data.runtime}</p>
     <p>Cast: ${data.cast}</p>
