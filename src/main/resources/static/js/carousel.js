9b5ec796cd4087863ea646dac1d0f617eb6a0946
shailynch <shai.lynch.sl@gmail.com>
2022-09-09 09:01:29 +0100

    Update carousel.js

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -1,54 +1,38 @@
-loadCarousel = ()=> {
-	document.getElementById("myCarousel").innerHTML = `
-		<div class="carousel-indicators">
-	              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-	              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-	              <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-	            </div>
-	            <div class="carousel-inner">
-	              <div class="carousel-item active">
+const loadCarousel = () => {
+		const filmURL = "http://18.132.36.167:8080/movie";
+	 let html = " ";
+	fetch(`${filmURL}/all`)
+     .then((response) => {
+         if (response.status !== 200) {
+             console.log(`Looks like there was a problem.Status Code: ${ response.status }`);
+             return;
+         }
+     response.json()
+     .then(movies => movies.forEach(movie => {
+		if(movie.new_release == true ){
+			html = html + ` 
+	          
+	              <div class="carousel-item">
 	                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" /></svg>
 	        
 	                <div class="container">
 	                  <div class="carousel-caption text-start">
-	                    <h1>Example Film headline.</h1>
-	                    <p>Film description</p>
-	                    <p><a class="btn btn-lg btn-primary" href="#">Film poster</a></p>
+	                    <h1>${movie.title}.</h1>
+	                    <p>${movie.description}</p>
+	                    <p><a class="btn btn-lg btn-primary" href="./individualFilm.html?${e.id}">
+                                <img class="img" src=${e.posterUrl} />
+                            </a></p>
 	                  </div>
 	                </div>
 	              </div>
-	              <div class="carousel-item">
-	                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-	        
-	                <div class="container">
-	                    <div class="carousel-caption text-start">
-	                        <h1>Example Film headline.</h1>
-	                        <p>Film description</p>
-	                        <p><a class="btn btn-lg btn-primary" href="#">Film poster</a></p>
-	                    </div>
-	                </div>
-	              </div>
-	              <div class="carousel-item">
-	                <svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></svg>
-	        
-	                <div class="container">
-	                    <div class="carousel-caption text-start">
-	                        <h1>Example Film headline.</h1>
-	                        <p>Film description</p>
-	                        <p><a class="btn btn-lg btn-primary" href="#">Film poster</a></p>
-	                    </div>
-	                </div>
-	              </div>
-	              </div>
-	            <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-	              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-	              <span class="visually-hidden">Previous</span>
-	            </button>
-	            <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-	              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-	              <span class="visually-hidden">Next</span>
-	            </button>
+	            
 	`	
+		}
+ 
+ 
+        document.getElementById("newReleases").innerHTML = html;
+}))
+     .catch(err => console.error(`Fetch Error :-S ${err}`));
+     });
 }
-
-loadCarousel();
+	   readFilms();
